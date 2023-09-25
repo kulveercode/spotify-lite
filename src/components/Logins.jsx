71e118c1b1e5556ef688928b0ea19707fd58df2d
Kulveer Singh <kulveer.coder@gmail.com>
2023-09-26 00:35:07 +0530

    second commit

---
+++ src/components/Logins.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logins = () => {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default Logins;
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { StateProvider } from './assets/utils/StateProvider.jsx';
+import reducer, { initialState } from './assets/utils/reducer.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <StateProvider initialState={initialState} reducer={reducer}>
     <App />
+  </StateProvider>
   </React.StrictMode>,
 );
 
