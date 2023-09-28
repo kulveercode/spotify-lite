fb1eccdde85b061afc7e08a9e25c5587ddddff9d
Kulveer Singh <kulveer.coder@gmail.com>
2023-09-28 22:39:34 +0530

    spofity started

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Logins from './components/Logins';
+import { reducerCases } from './utils/Constants';
+import Spotify from "./components/Spotify";
+import { useStateProvider} from "./utils/StateProvider";
 
 
 function App() {
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
 return (
-    <>
       <div>
-        <Logins />
+      {token ? <Spotify /> : <Logins />}
       </div>
-    </>
-  )
+  );
 }
 
 export default App;
--- src/components/Logins.jsx
+++ src/components/Logins.jsx
@@ -3,8 +3,22 @@ import styled from 'styled-components';
 
 const Logins = () => {
   const handleClick = () => {
-    alert("heelo");
-  }
+    const clientId = "8d2b8286c0e24639bde68c084df462c1";
+    const redirectUrl = "http://localhost:5173/";
+    const apiUrl = "https://accounts.spotify.com/authorize";
+    const scope = [
+      "user-read-email",
+      "user-read-private",
+      "user-read-playback-state",
+      "user-modify-playback-state",
+      "user-read-currently-playing",
+      "user-read-playback-position",
+      "user-top-read",
+      "user-read-recently-played"
+    ];
+    window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(" ")
+  }&response_type=token&show_daialog=true`;
+  };
 
   return (
     <Container>
